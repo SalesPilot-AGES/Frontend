43c65441b7f01399927185d40ee40965749cebd1
KillianDB <killiandomingues57@gmail.com>
2026-04-15 07:45:44 -0300

    fix: import error

--- src/UI/DataTable/DataTableToolbar/DataTableToolbar.tsx
+++ src/UI/DataTable/DataTableToolbar/DataTableToolbar.tsx
@@ -12,8 +12,8 @@ import {
 import type { Palette } from '@mui/material/styles';
 import type { JSX } from 'react';
 
+import type { DataTableSurfaceColors } from '../../../hooks/useDataTable';
 import type { DataTableFilterOption } from '../../../types/ui';
-import type { DataTableSurfaceColors } from '../../../UI/DataTable/DataTable';
 
 export interface DataTableToolbarProps {
   toolbarTitle?: string;

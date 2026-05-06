00a1bdb0ee0929c4a7a7344f161558cc00ffa22a
Caetano Hillesheim Sadosiuk <hillcaetano@gmail.com>
2026-05-05 23:53:04 -0300

    fix: remove button for tests

--- src/pages/MeetingsPage/SalesmanMeetingsPage/SalesmanMeetingsPage.tsx
+++ src/pages/MeetingsPage/SalesmanMeetingsPage/SalesmanMeetingsPage.tsx
@@ -1,7 +1,5 @@
 import { EpageDescriptions } from '@data/enums/EpageDescriptions';
 import { EPageTitles } from '@data/enums/EPageTitles';
-import { Button } from '@mui/material';
-import { Link } from '@tanstack/react-router';
 import { PageContainter } from '@UI/PageContainer/PageContainer';
 import { PageHeader } from '@UI/PageHeader/PageHeader';
 import type { JSX } from 'react';
@@ -13,17 +11,6 @@ export const SalesmanMeetingsPage = (): JSX.Element => {
         title={EPageTitles.MEETINGS}
         subtitle={EpageDescriptions.MEETINGS}
       />
-
-      {/* BOTÃO TEMPORÁRIO PARA TESTAR A NAVEGAÇÃO */}
-      <Button
-        component={Link}
-        to="/vendedores/reuniões/m1a2b3c4-d5e6-7890-1234-56789abcdef0"
-        variant="contained"
-        color="primary"
-        sx={{ mt: 4 }}
-      >
-        IR PARA A TELA DE DETALHE (TESTE)
-      </Button>
     </PageContainter>
   );
 };
